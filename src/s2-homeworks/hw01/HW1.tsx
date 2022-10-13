6ec9b13fc56fbc1ca9a565a67fcd7ae4a9cf4e2e
Vlad Matsuk <matsukvlad399@gmail.com>
2022-10-13 18:59:18 +0300

    Add HW1 and new package.json

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -6,29 +6,42 @@ import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
 /*
-* 1 - описать тип MessageType
+* 1 - описать тип MessageType- сделал!!!!
 * 2 - описать тип MessagePropsType в файле Message.tsx
 * 3 - в файле Message.tsx отобразить приходящие данные
 * 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
 * 5 - сделать стили в соответствии с дизайном
 * */
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+type UserType = {
+    avatar: string,
+    name: string
+}
+
+type MessageType = {
+    text: string,
+    time: string
+}
+
+export type MessagesType = {
+    id: number,
+    user: UserType,
+    message: MessageType
+}
 
 // структуру объекта не менять
-export const message0: MessageType = {
+export const message0: MessagesType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Vladislav',  // можно менять
     },
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
     },
 }
-export const friendMessage0: MessageType = {
+export const friendMessage0: MessagesType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
@@ -47,12 +60,12 @@ const HW1 = () => {
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
-                    <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
+                    <Message message={message0}/>
+                    <FriendMessage message={friendMessage0}/>
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
+                <MessageSender M={Message}/>
             </div>
         </div>
     )
