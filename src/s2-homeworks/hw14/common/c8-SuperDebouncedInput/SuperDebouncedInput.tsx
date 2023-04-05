aaf8af95b8a8dca376c518ed751805dd8010f308
Vladislav110 <matsukvlad399@gmail.com>
2023-04-05 11:29:45 +0300

    Add HW-13

---
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -0,0 +1,51 @@
+import React, {DetailedHTMLProps, InputHTMLAttributes, ReactNode, useState} from 'react'
+import SuperInputText from '../../../hw04/common/c1-SuperInputText/SuperInputText'
+
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement>
+
+
+export type SuperDebouncedInputPropsType = Omit<DefaultInputPropsType, 'type'> & {
+
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: ReactNode
+    spanClassName?: string
+} // илм экспортировать тип SuperInputTextPropsType
+    & { // плюс специальный пропс SuperPagination
+    onDebouncedChange?: (value: string) => void
+}
+
+const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
+    {
+        onChangeText,
+        onDebouncedChange,
+
+        ...restProps
+    }
+) => {
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | undefined>(undefined);
+
+    const onChangeTextCallback = (value: string) => {
+        onChangeText?.(value)
+
+        if (onDebouncedChange) {
+            if (timerId) {
+                clearTimeout(timerId)
+            }
+
+            const newTimerId = setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500)
+
+            setTimerId(newTimerId)
+}
+    }
+
+    return (
+        <SuperInputText onChangeText={onChangeTextCallback} {...restProps}/>
+    )
+}
+
+export default SuperDebouncedInput
